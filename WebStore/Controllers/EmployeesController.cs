d49197c560ea2c3c166041acb2c05cb6c8a289d1
Artemii <chekushin@hotmail.com>
2019-11-19 19:35:28 +0300

    Домашка по третьему уроку: осталось добавить вывод информации об ошибках

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using WebStore.ViewModels;
+using WebStore.Infrastructure.Interfaces;
 
 
 namespace WebStore.Controllers
@@ -12,28 +13,28 @@ namespace WebStore.Controllers
     public class EmployeesController : Controller
     {
         private readonly IConfiguration _Configuration;
-        public EmployeesController(IConfiguration Configuration) => _Configuration = Configuration;
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesController(IEmployeesData EmployeesData, IConfiguration Configuration)  {
+            _EmployeesData = EmployeesData;
+            _Configuration = Configuration;
+        }
+        
         public IActionResult ReadConfig() => Content(_Configuration["CustomData"]);
         public IActionResult Index()
         {
-            return View("GetEmployes", __Employees);
+            return View("GetEmployes", _EmployeesData.GetAll());
         }
          
-        private static readonly List<EmployeeView> __Employees = new List<EmployeeView>
-        {
-            new EmployeeView { Id = 1, SecondName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 35, BirtDay = new DateTime(1984, 8, 20) },
-            new EmployeeView { Id = 2, SecondName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 25, BirtDay = new DateTime(1994, 7, 19) },
-            new EmployeeView { Id = 3, SecondName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18, BirtDay = new DateTime(2001, 6, 18) },
-        };
         public IActionResult GetEmployes()
         {
-            return View(__Employees);
+            return View(_EmployeesData.GetAll());
         }
 
         public IActionResult GetEmployee(int employeeId)
         {
-            var employee = __Employees.Any(it => it.Id == employeeId) ?
-                __Employees.First(it => it.Id == employeeId) :
+            var employee = _EmployeesData.GetAll().Any(it => it.Id == employeeId) ?
+                _EmployeesData.GetAll().First(it => it.Id == employeeId) :
                 new EmployeeView { Id = 0, SecondName = "Нет", FirstName = "и не может быть", Patronymic = "такого сотрудника", Age = 35, BirtDay = new DateTime(1984, 8, 20) };
             return View(employee);
         }
@@ -45,25 +46,49 @@ namespace WebStore.Controllers
         [HttpPost]
         public IActionResult AddNewEmployee(EmployeeView employee)
         {
-            employee.Id = __Employees.Count() + 1;
-            __Employees.Add(employee);
+            employee.Id = _EmployeesData.GetAll().Count() + 1;
+            _EmployeesData.Add(employee);
             return RedirectToAction(nameof(Index));
         }
 
         public IActionResult EditEmployee(int emploeeId)
         {
-            var employee = __Employees.First(it => it.Id == emploeeId);
+            var employee = _EmployeesData.GetAll().First(it => it.Id == emploeeId);
             return View(employee);
         }
         [HttpPost]
         public IActionResult EditEmployee(EmployeeView employee)
         {
-            __Employees.RemoveAt(__Employees.IndexOf(
-                __Employees.First(it => it.Id == employee.Id)
-                )
-            );
-            __Employees.Add(employee);
-            return RedirectToAction(nameof(Index));
+            if (employee is null)
+                throw new ArgumentOutOfRangeException(nameof(employee));
+
+            if (!ModelState.IsValid)
+                View(employee);
+
+            var id = employee.Id;
+            if (id == 0)
+                _EmployeesData.Add(employee);
+            else
+                _EmployeesData.Edit(id, employee);
+
+            _EmployeesData.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var employee = _EmployeesData.GetById(Id);
+            if (employee is null)
+                return NotFound();
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int Id)
+        {
+            _EmployeesData.Delete(Id);
+            return RedirectToAction("Index");
         }
     }
 }
--- WebStore/ViewModels/EmployeeView.cs
+++ WebStore/ViewModels/EmployeeView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,15 @@ namespace WebStore.ViewModels
     public class EmployeeView
     {
         public int Id { get; set; }
-
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
-
+        [Display(Name = "Фамилия")]
         public string SecondName { get; set; }
-
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
-
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
+        [Display(Name = "Дата рождения")]
         public DateTime BirtDay { get; set; }
     }
 }
